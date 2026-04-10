2f44465f535e5577fb8b675d7df65c8dac4997d0
AlejandroAMG <alejandrogomezmesa1@gmail.com>
2026-04-10 00:11:33 -0500

    SQLITE3

--- proyecto_pedidos/settings.py
+++ proyecto_pedidos/settings.py
@@ -85,12 +85,8 @@ WSGI_APPLICATION = 'proyecto_pedidos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pedidos',
-        'USER': 'root',
-        'PASSWORD': 'Alejandro15',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
